5e71a11bff429f360e02d885b063228f0a0782a7
Abenet21 <abinetzerihun19@gmail.com>
2025-07-13 11:20:18 +0300

    Describe what you changed here

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Netflix-by-alex/',  
   plugins: [react()],
-});
+  base: '/Netflix-by-alex/', // 👈 This is required for GitHub Pages
+})
